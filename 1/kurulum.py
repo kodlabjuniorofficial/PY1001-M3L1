079e2304c14f465bf31cd42c943245eff5d277fb
kodlabjuniorofficial <kodlabjuniorofficial@gmail.com>
2026-01-18 17:50:41 +0300

    Create kurulum.py with setup instructions
    Add installation instructions for Telegram bot

---
+++ 1/kurulum.py
@@ -0,0 +1,10 @@
+"""
+python telegram botu için jurulum komutları
+pip install python-telegram-bot 
+telegramda @botfather botunu bulup yeni bot oluşturup token alın
+kullanacağınız komut botname_bot
+sonra sizler ile oluşturduğumuz bota bu kendi botunuzu ekleyiniz 
+sonra yazdığınız komutları /satart vs deneyiniz
+
+
+"""
